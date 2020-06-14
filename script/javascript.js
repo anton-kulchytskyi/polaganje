0aebd0d9ba76d5554834ebc80f1e682818ec8613
anton-kulchytskyi <anton@oykos-hub.com>
2020-06-14 14:39:57 +0200

    folders

---
+++ script/javascript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var input1 = parseInt(document.getElementById('phone').value);
+	alert(input1);
+}
+function myFunctionFooter() {
+    var input2 = parseInt(document.getElementById('phoneFooter').value);
+	alert(input2);
+}
